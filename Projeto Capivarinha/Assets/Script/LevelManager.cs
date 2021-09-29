75e5463dcd0186494586d22756db68f3112e44d4
lohannahme <61636785+lohannahme@users.noreply.github.com>
2021-09-28 23:18:17 -0300

    Loading Scene

---
+++ Projeto Capivarinha/Assets/Script/LevelManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    [SerializeField] private GameObject _menuCanvas;
+    [SerializeField] private GameObject _loadCanvas;
+    public Slider slider;
+
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        _menuCanvas.SetActive(false);
+        _loadCanvas.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+
+            yield return null;
+
+        }
+
+        _loadCanvas.SetActive(false);
+    }
+
+   
+}
